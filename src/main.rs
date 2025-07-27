f3d13f3d163c2cb29d36d69e361cfe8cbb989a5f
Steve Cirelli <scirelli+git@gmail.com>
2025-07-26 21:35:49 -0400

    quick save

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[derive(Debug)]
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user = User{
+        active: true,
+        username: String::from("Hello"),
+        email: String::from("bob@bobsicle@gmail.com"),
+        sign_in_count: 0
+    };
+    let user2 = User{
+        ..user
+    };
+    println!("{user:?}\n{user2:?}");
 }
